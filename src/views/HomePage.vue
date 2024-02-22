9a36e5ccef8e40e228a3a200a39238580ac3c55f
runny <watcharit_s@cmu.ac.th>
2024-02-22 22:37:20 +0700

    comitteee

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -3,9 +3,22 @@ import HomepageCarousel from '@/components/HomepageCarousel.vue'
 </script>
 <template>
     <div class="body">
-    <HomepageCarousel></HomepageCarousel>
+      <HomepageCarousel></HomepageCarousel>
     </div>
-    <h1>asdasd</h1>
-
-</template>
-
+    <div class="full-screen-image">
+      <img src="https://cdn.thewirecutter.com/wp-content/media/2021/04/52things-2048px-8670-3x2-1.jpg?auto=webp&quality=75&crop=3:2&width=1024" alt="">
+    </div>
+  </template>
+  
+  <style scoped>
+  .full-screen-image {
+    width: 100%;
+    height: 100vh; /* Set height to 100% of the viewport height */
+  }
+  
+  .full-screen-image img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover; /* Maintain aspect ratio while covering the container */
+  }
+  </style>
